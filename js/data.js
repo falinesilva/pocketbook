586c8e811a59e59c602717ef20beb94956765048
falinesilva <faline.silva@proton.me>
2025-10-07 13:17:03 -0300

    changing logic of ux

--- js/data.js
+++ js/data.js
@@ -1,19 +1,26 @@
-const bills = [
-  { dueDate: "2025-09-02", status: "PAID", name: "Crunchyroll", category: "Subscription", amount: 14.90 },
-  { dueDate: "2025-09-08", status: "PAID", name: "Consorcio", category: "Investments", amount: 332.00 },
-  { dueDate: "2025-09-08", status: "LATE", name: "Phone", category: "Bill", amount: 85.00 },
-  { dueDate: "2025-09-08", status: "PAID", name: "Veterinarian", category: "Debt", amount: 600.00 },
-  { dueDate: "2025-09-10", status: "PAID", name: "Music Class", category: "Investments", amount: 175.00 },
-  { dueDate: "2025-09-10", status: "LATE", name: "Electricity", category: "Bill", amount: null },
-  { dueDate: "2025-09-23", status: "PAID", name: "Credit Card", category: "Debt", amount: 400.00 },
+// All past due and currently due
+const due = [
+  { totalDue: 1643.73, dueDate: "TBD", name: "IPVA", category: "Tax"},
+  { totalDue: 254.39, dueDate: "TBD", name: "MEI", category: "Tax"},
+];
+window.due = due;
+
+// The next or upcoming
+const next = [
+  { totalDue: 450.00, dueDate: "Nov. 1st 2025", name: "Chicken Wire", category: "Loan"},
+  { totalDue: 2500.00, dueDate: "Nov. 23rd 2025", name: "NuBank", category: "Credit Card" },
+  { totalDue: 340.00, dueDate: "Nov. 8th 2025", name: "Consorcio", category: "Investments"},
+  { totalDue: 82.00, dueDate: "Nov. 8th 2025", name: "Phone", category: "Bill" },
+  { totalDue: null, dueDate: "Nov. 10th 2025", name: "Electricity" },
+  { totalDue: 348.00, dueDate: "Jan. 8th 2025", name: "Consorcio", category: "Investments"},
 ];
 
-const debts = [
-  { totalDue: 600.00, plan: "Monthly", name: "Veterinarian", type: "Loan", estimated: false},
-  { totalDue: 4000.00, plan: "Monthly", name: "NuBank", type: "Credit Card", estimated: true },
-  { totalDue: 450.00, plan: "TBD", name: "Chicken Wire", type: "Loan", estimated: false },
-  { totalDue: 1643.73, plan: "TBD", name: "IPVA", type: "Tax", estimated: false },
-  { totalDue: 254.39, plan: "TBD", name: "MEI", type: "Tax", estimated: false },
-
-  
+// The next 30+ days
+const soon = [
+  { totalDue: 450.00, dueDate: "Nov. 1st 2025", name: "Chicken Wire", category: "Loan"},
+  { totalDue: 2500.00, dueDate: "Nov. 23rd 2025", name: "NuBank", category: "Credit Card" },
+  { totalDue: 340.00, dueDate: "Nov. 8th 2025", name: "Consorcio", category: "Investments"},
+  { totalDue: 82.00, dueDate: "Nov. 8th 2025", name: "Phone", category: "Bill" },
+  { totalDue: null, dueDate: "Nov. 10th 2025", name: "Electricity" },
+  { totalDue: 348.00, dueDate: "Jan. 8th 2025", name: "Consorcio", category: "Investments"},
 ];
